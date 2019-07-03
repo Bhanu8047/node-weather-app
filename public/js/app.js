da8b837a410420693be201b3e7bc4262225537ee
bhanu8047 <bpsbhanupratap96@gmail.com>
2019-07-04 02:19:42 +0530

    removed errors

--- public/js/app.js
+++ public/js/app.js
@@ -1,5 +1,3 @@
-console.log('client side javascript!')
-
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('#address')
 const message1 = document.querySelector('#message1')
