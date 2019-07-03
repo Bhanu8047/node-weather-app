b239a467bcd2b9f52dc6025ce3a535752fdc4d65
bhanu8047 <bpsbhanupratap96@gmail.com>
2019-07-04 03:36:17 +0530

    Details Added

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,7 @@ const forecast = (lat, long, callback) => {
         } else if(body.error) {
             callback('Unable to find the location ! Try again.', undefined)
         } else {
-            callback(undefined, `${body.daily.data[0].summary} It is currently ${body.currently.temperature} degrees out here. There is a ${body.currently.precipProbability*100}% chance of rain.`)
+            callback(undefined, `${body.daily.data[0].summary} It is currently ${body.currently.temperature} F degrees out here. There is a ${body.currently.precipProbability*100}% chance of rain. And also the highest temperature could be: ${body.daily.data[0].temperatureHigh} F and lowest could be: ${body.daily.data[0].temperatureLow} F with ${body.daily.data[0].humidity} humidity index.`)
         }
     })
 }
